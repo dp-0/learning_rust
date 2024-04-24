4a51384b0d9b7cdc9a9265010f053f63e60efaf0
Dinesh Phuyel <86118075+dp-0@users.noreply.github.com>
2024-04-24 12:40:52 +0545

    level1 print command

--- src/main.rs
+++ src/main.rs
@@ -7,5 +7,15 @@ fn main() {
  comment
  */
 
- print!("Hello from rust");
+ println!("lets display a dynamic message i.e number =  {}", 10);
+
+ //learnig to print string
+
+println!("my name is {} , my last name is {}","Dinesh","Phuyel");
+
+//learning the print
+print!("there is no line break");
+print!("This is next line");
+print!("o i get everything on same line");
+
 }
